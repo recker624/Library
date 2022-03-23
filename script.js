b638dceaa187808dd56b54b9c6e81aa21abd907d
recker624 <koolcephoid@gmail.com>
2022-03-23 14:06:53 +0530

    Created html structure for book card

--- script.js
+++ script.js
@@ -2,21 +2,71 @@
 
 let myLibrary = [];
 
+let formContainer = document.querySelector('.container');
+let form = document.querySelector("form");
+let addBookBtn = document.querySelector(".library .add-btn");
+let addFormBtn = document.querySelector("form .add button");
+let closeFormBtn = document.querySelector("form .cancel-btn")
+let overlay = document.querySelector(".overlay");
+
+addBookBtn.addEventListener("click", addBookToLibrary);
+addFormBtn.addEventListener("click", addBookForm);
+closeFormBtn.addEventListener("click", closeBookForm);
+
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = (isRead == "yes") ? true : (isRead == "no" ? false : "error");
+  this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-   let title, author, pages, isRead;
- 
-   title = prompt("Enter book title: ", "title");
-   author = prompt("Enter book author: ", "author name");
-   pages = +prompt("Enter number of pages in book: ", 0);
-   isRead = prompt("Have you already read the book? (Yes/No): ").toLowerCase();
+//"add" button in form
+function addBookForm(event) {
+  event.preventDefault();
 
-   let book = new Book(title, author, pages, isRead);
-   myLibrary.push(book);
+  let title, author, pages, isRead;
+
+  title  = form.querySelector("#title").value;
+  author = form.querySelector("#author").value;
+  pages  = form.querySelector("#pages").value;
+  isRead = form.querySelector("#read-status").checked;
+
+  let book = new Book(title, author, pages, isRead);
+  myLibrary.push(book);
+  formContainer.style.visibility = "hidden";
+
+  overlay.classList.remove("active");
+
+  createBookCard();
+}
+
+//main button for adding books to library
+function addBookToLibrary() {
+  formContainer.style.visibility = "visible";
+  overlay.classList.add("active");
+  form.style.backgroundColor = "#fff";
+}
+
+//"close" button in form
+function closeBookForm() {
+  formContainer.style.visibility = "hidden";
+  overlay.classList.remove("active");
+}
+
+function createBookCard() {
+  let latestBook  = myLibrary[myLibrary.length-1];
+  let newBookCard = document.createElement("div");
+
+  let bookTitle   = document.createElement("div");
+  let preposition = document.createElement("div");
+  let bookAuthor  = document.createElement("div");
+  let pages       = document.createElement("div");
+  let read        = document.createElement("div");
+  let notRead     = document.createElement("div");
+  let deleteCard  = document.createElement("div");
+
+  
+
+  newBookCard.classList.add("book-card");
+  addBookBtn.before(newBookCard);
 }
